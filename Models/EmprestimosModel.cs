7b03d4b6888495867849335b9a363ea7bfb3db73
Leandro Martins <leandromartinspinto2004@gmail.com>
2024-10-30 16:07:50 -0300

    Instalando os pacotes

---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Biblioteca.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+    }
+}
